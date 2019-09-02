a5f1610915aabbece4943cbee23475db57ad0ae2
leHoangKhacSon <sonqueen.no1@gmail.com>
2019-09-02 15:19:04 +0700

    Add controllers folder

---
+++ controllers/user.controller.js
@@ -0,0 +1,45 @@
+// chua cac function xu ly users
+var db = require('../db');
+var shortid = require('shortid');
+// exports function index
+module.exports.index = function(req, res){
+	res.render('users/index', {
+		users: db.get('users').value()
+	});
+}; 
+// exports function search
+module.exports.search = function(req, res){
+	let q = req.query.q;
+	let matchedUsers = db.get('users').value().filter(function(user){
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('users/index', {
+		users: matchedUsers,
+	});
+};
+// exports function create
+module.exports.create = function(req, res){
+	res.render('users/create')
+};
+// exports function get
+module.exports.get = function(req, res){
+	// tim id dua vao req.params
+	let id = req.params.id;
+	// tim user dua vao id tren
+	const user = db.get('users').find({id: id}).value();
+	res.render('users/view', {
+		user: user
+	});
+};
+// exports function postCreate
+module.exports.postCreate = function(req, res){
+	console.log(req.body);
+	console.log(db.get('users').value());
+	// tao id ngau nhien cho req.body
+	req.body.id = shortid.generate();
+	// them du lieu vao database
+	db.get('users')
+	  .push(req.body)
+	  .write();
+	res.redirect('/users')
+};
