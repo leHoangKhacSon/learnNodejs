3feb135b34c8b96e25e20caa915656185e532c0d
leHoangKhacSon <sonqueen.no1@gmail.com>
2019-09-03 17:41:26 +0700

    Create panigation

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,7 +1,20 @@
 const db = require('../db.js');
 
 module.exports.index = function(req, res){
+	// lấy về page hiện tại mặc định bằng 1
+	let page = parseInt(req.query.page) || 1;
+	// 8 sản phẩm trong 1 trang
+	let perPage = 8;
+
+	let start = (page -1) * perPage;
+	let end = page * perPage;
+
+	let drop = (page -1) * perPage;
+
 	res.render('products/index', {
-		products: db.get('products').value()
+		// cách 1
+		// products: db.get('products').value().slice(start, end)
+		// cách 2
+		products: db.get('products').drop(drop).take(perPage).value()
 	});
 };
