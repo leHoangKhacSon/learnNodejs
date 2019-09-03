53ab0aa6d92eac98bb4a1c1f09a5d3a6ccec8455
leHoangKhacSon <sonqueen.no1@gmail.com>
2019-09-04 02:39:16 +0700

    Add item-page panigation

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -9,12 +9,32 @@ module.exports.index = function(req, res){
 	let start = (page -1) * perPage;
 	let end = page * perPage;
 
+	// code page-item phân trang
+	// đếm tất cả sản phẩm trong csdl
+	let product = db.get('products').value().length;
+	let maxPage = Math.ceil(product/perPage);
+	// tạo mảng - chứa 3 phần tử của page-item
+	let pageArray = [];
+	if(page <= 1){
+		page = 1;
+		pageArray = [1, 2, 3];
+	}else {if(page >= maxPage){
+			pageArray = [maxPage-2, maxPage-1, maxPage];
+		}else {
+			pageArray = [page-1, page, page+1];
+		}}
+
+	// 	
 	let drop = (page -1) * perPage;
 
 	res.render('products/index', {
 		// cách 1
 		// products: db.get('products').value().slice(start, end)
 		// cách 2
-		products: db.get('products').drop(drop).take(perPage).value()
+		products: db.get('products').drop(drop).take(perPage).value(),
+		pageArray: pageArray,
+		page: page
 	});
+	// pagination
+
 };
