dccb07efbfe7fb0c6cc20d860e965270ebab5de2
leHoangKhacSon <sonqueen.no1@gmail.com>
2019-09-11 09:48:33 +0700

    Create API put product

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -21,3 +21,55 @@ module.exports.create = async function(req, res){
 		})
 	}
 }
+
+module.exports.putProduct = async function(req, res, next){
+	// lay id nguoi dung put len
+	let _id = req.body._id;
+	// tim xem da co san pham hay chua
+	let product = await Product.findOne({_id: _id});
+	if(product){
+		let newProduct = await Product.findByIdAndUpdate({_id: _id}, {
+			name: req.body.name,
+			price: req.body.price,
+			image: req.body.image,
+			description: req.body.description
+		});
+		if(newProduct){
+			res.json({
+				result: "edit",
+				data: newProduct,
+				message: "edit product success"
+			});
+		} else {
+			res.json({
+				result: "edit failure",
+				data: "",
+				message: "edit product failure"
+			});
+		}
+		
+	} 
+	else {
+		let newProduct = await Product.create({
+			name: req.body.name,
+			price: req.body.price,
+			image: req.body.image,
+			description: req.body.description
+		});
+
+		if(newProduct){
+			res.json({
+				result: "create",
+				data: newProduct,
+				message: "create product success"
+			});
+		} else {
+			res.json({
+				result: "create failure",
+				data: "",
+				message: "create product failure"
+			});
+		}
+	}
+}
+
